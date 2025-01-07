90cb95b9d0ec99fe6532e3f5137658424166260d
Nawamin ICE <157040537+Nanomete@users.noreply.github.com>
2025-01-07 12:27:45 +0700

    add NO_1.java

---
+++ 6721651475_Nawamin/Lab_6/NO_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NO_1 {
+    // ข้อ 1
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input number : ");
+        int x = input.nextInt();
+        int arr[][] = new int[x][];
+        int nCol = x;
+        int count = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            // nCol = x, x-1, x-2, ..., 1
+            arr[i] = new int[nCol];
+            nCol--;
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = count;
+                count++;
+            }
+        }
+
+        for (int[] row : arr) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
